fc1344d6ef8e3c1553e6f4930a2d1dbffe2e3e0c
BARNOCH Anas <anasbarnoch@gmail.com>
2025-11-16 18:03:32 +0100

    Integrate extern mcp server tools

--- src/main/java/org/example/mcpserver/records/Product.java
+++
@@ -1,9 +0,0 @@
-package org.example.mcpserver.records;
-
-public record Product(
-        String name,
-        String description,
-        double price,
-        int quantity
-) {
-}
--- src/main/java/org/example/mcpserver/web/AIAgentController.java
+++ src/main/java/org/example/mcpserver/web/AIAgentController.java
@@ -1,26 +1,32 @@
 package org.example.mcpserver.web;
 
-import org.example.mcpserver.tools.AITools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/api")
 public class AIAgentController {
     private final ChatClient chatClient;
 
-    public AIAgentController(ChatClient.Builder builder, ChatMemory memory, AITools aiTools) {
+    public AIAgentController(ChatClient.Builder builder, ChatMemory memory, ToolCallbackProvider tools) {
+        Arrays.stream(tools.getToolCallbacks()).forEach(callbackTool -> {
+            System.out.println("========tools=========");
+            System.out.println(callbackTool.getToolDefinition().name());
+        });
         this.chatClient = builder
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(memory).build()
                 )
-                .defaultTools(aiTools)
+                .defaultToolCallbacks(tools)
                 .build();
     }
 
